9e500529e4a6aa1fa27b71f59b27f25cf4778a87
GarudaFX <email@francistinao.tech>
2023-12-04 13:27:16 +0800

    feat: added hook for fetching data

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
+
+import { StudentAttendance } from '@/types/global';
+
+export const useFetch = (url: string, token: string) => {
+  const [data, setData] = useState<StudentAttendance[]>([]);
+
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${token}`
+  } as HeadersInit);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url, {
+        headers
+      });
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, [url]);
+
+  return { data };
+};
