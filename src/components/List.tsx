ce4753aad9e1c0908ba6e1407a10b0149c9fcc71
dimplestinto <dimples.tinto@gmail.com>
2023-12-19 00:14:44 +0800

    refactor: added checking of students' list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -42,78 +42,86 @@ const List: React.FC = () => {
 
   return (
     <div className="flex flex-col gap-4 h-full">
-      {students?.map((student, idx) =>
-        loading ? (
-          <Skeleton variant="rectangular" width={'100%'} height={40} />
-        ) : (
-          <div
-            key={idx}
-            className="bg-transparent w-full rounded-md p-2 border-t border-zinc-400"
-          >
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between items-center">
-                <div className="flex gap-4 items-center">
-                  <FaCircleUser className="text-primary" size={25} />
-                  <div className="flex flex-col">
-                    <h1 className="font-semibold">
-                      {student?.first_name + ' ' + student?.last_name}
-                    </h1>
-                    <p className="text-xs">{student?.program_name}</p>
+      {students?.length !== 0 ? (
+        students?.map((student, idx) =>
+          loading ? (
+            <Skeleton variant="rectangular" width={'100%'} height={40} />
+          ) : (
+            <div
+              key={idx}
+              className="bg-transparent w-full rounded-md p-2 border-t border-zinc-400"
+            >
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-4 items-center">
+                    <FaCircleUser className="text-primary" size={25} />
+                    <div className="flex flex-col">
+                      <h1 className="font-semibold">
+                        {student?.first_name + ' ' + student?.last_name}
+                      </h1>
+                      <p className="text-xs">{student?.program_name}</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <LiaEmpire
+                      size={15}
+                      className={`${
+                        student?.year_level_code === '1st'
+                          ? 'text-green-600'
+                          : student?.year_level_code === '2nd'
+                          ? 'text-blue-600'
+                          : student?.year_level_code === '3rd'
+                          ? 'text-red-600'
+                          : student?.year_level_code === '4th'
+                          ? 'text-yellow-500'
+                          : 'text-purple-500'
+                      }`}
+                    />
+                    <p
+                      className={`${
+                        student?.year_level_code === '1st'
+                          ? 'text-green-600'
+                          : student?.year_level_code === '2nd'
+                          ? 'text-blue-600'
+                          : student?.year_level_code === '3rd'
+                          ? 'text-red-600'
+                          : student?.year_level_code === '4th'
+                          ? 'text-yellow-500'
+                          : 'text-purple-500'
+                      }`}
+                    >
+                      {student?.year_level_code}
+                    </p>
                   </div>
                 </div>
-                <div className="flex gap-2 items-center">
-                  <LiaEmpire
-                    size={15}
-                    className={`${
-                      student?.year_level_code === '1st'
-                        ? 'text-green-600'
-                        : student?.year_level_code === '2nd'
-                        ? 'text-blue-600'
-                        : student?.year_level_code === '3rd'
-                        ? 'text-red-600'
-                        : student?.year_level_code === '4th'
-                        ? 'text-yellow-500'
-                        : 'text-purple-500'
-                    }`}
-                  />
-                  <p
-                    className={`${
-                      student?.year_level_code === '1st'
-                        ? 'text-green-600'
-                        : student?.year_level_code === '2nd'
-                        ? 'text-blue-600'
-                        : student?.year_level_code === '3rd'
-                        ? 'text-red-600'
-                        : student?.year_level_code === '4th'
-                        ? 'text-yellow-500'
-                        : 'text-purple-500'
-                    }`}
-                  >
-                    {student?.year_level_code}
+                <div className="flex gap-2 items-center ml-10">
+                  <p className="bg-zinc-400 p-2 rounded-md text-xs ">
+                    Time In:{' '}
+                    {new Date(student?.time_in).toLocaleTimeString('en-US', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: true
+                    })}
+                  </p>
+                  <p className="bg-zinc-400 p-2 rounded-md text-xs ">
+                    Time Out:{' '}
+                    {new Date(student?.time_out).toLocaleTimeString('en-US', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: true
+                    })}
                   </p>
                 </div>
               </div>
-              <div className="flex gap-2 items-center ml-10">
-                <p className="bg-zinc-400 p-2 rounded-md text-xs ">
-                  Time In:{' '}
-                  {new Date(student?.time_in).toLocaleTimeString('en-US', {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true
-                  })}
-                </p>
-                <p className="bg-zinc-400 p-2 rounded-md text-xs ">
-                  Time Out:{' '}
-                  {new Date(student?.time_out).toLocaleTimeString('en-US', {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: true
-                  })}
-                </p>
-              </div>
             </div>
-          </div>
+          )
         )
+      ) : (
+        <div className="h-screen grid place-content-center place-items-center justify-center">
+          <h1 className="font-bold text-3xl text-center">
+            No students yet being scanned
+          </h1>
+        </div>
       )}
     </div>
   );
